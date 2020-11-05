63983a147e646e8ea4c9161e6d7ae39a75aa4682
Henrique Pereira <wafx.henrique@grupofleury.com.br>
2020-11-05 17:38:02 -0300

    config typeorm

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,33 +1,13 @@
+import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '../models/Appointment';
-import { isEqual } from 'date-fns';
 
-interface CreateAppointmentDto {
-  provider: string;
-  date: Date;
-}
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointments = await this.findOne({
+      where: { date },
+    });
 
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public create({ provider, date }: CreateAppointmentDto): Appointment {
-    const appointment = new Appointment({ provider, date });
-    this.appointments.push(appointment);
-
-    return appointment;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointments = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
     return findAppointments || null;
   }
 }
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,26 +1,25 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointMentRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointMentRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
     const parsedDate = parseISO(date);
 
-    const appointmentService = new CreateAppointmentService(
-      appointMentRepository,
-    );
-    const appointment = appointmentService.execute({
+    const appointmentService = new CreateAppointmentService();
+    const appointment = await appointmentService.execute({
       provider,
       date: parsedDate,
     });
